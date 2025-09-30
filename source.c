e0c462aa6cc244647fb6d57bb85597d12496cb9e
AvramenkoD <dsdautor227@gmail.com>
2025-09-30 17:02:01 +0300

    Добавьте файлы проекта.

---
+++ source.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	int numberA;
+	int numberB;
+	int rez;
+
+	setlocale(LC_ALL, "RUS");
+
+	puts("Введите номера ваших рабочих мест через пробел (А и В)...");
+	scanf("%d %d", &numberA, &numberB);
+
+	rez = (numberA % 2 == 1 && numberB % 2 == 0) || (numberB % 2 == 1 && numberA % 2 == 0);
+
+	printf("Вы пьёте кофе (1 - да; 0 - нет): %d\n",rez);
+
+	return 0;
+}
